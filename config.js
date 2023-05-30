799466644f7ef80385938b11902a3c3bb13c0673
usupal <udyan.upal@gmail.com>
2023-05-31 02:16:22 +0600

    .

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Dev Blog', // Required
+  author: 'UdYaN UpAL', // Required
+  description: 'Full-stack Web Developer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://usupal.vercel.app/',
+    github: 'https://github.com/usupal',
+    twitter: 'https://twitter.com/udyan_upal',
+    linkedin: 'https://linkedin.com/in/udyan-upal',
+  },
+  pathPrefix: '/devblog',
+  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+};
